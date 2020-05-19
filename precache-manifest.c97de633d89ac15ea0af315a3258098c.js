273b39a35d9e0e03c6646b6c8d49db153e53d3e7
mattiadaddazio <mattiadadda02@gmail.com>
2020-05-19 19:27:33 +0200

    Updates

---
+++ precache-manifest.c97de633d89ac15ea0af315a3258098c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7116fc59c2d5449275e6c4fda5417d7d",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "808aad98b777f71e7c7e",
+    "url": "/simple-calculator/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d8d3e135115131437979",
+    "url": "/simple-calculator/static/css/main.4f4abcb9.chunk.css"
+  },
+  {
+    "revision": "808aad98b777f71e7c7e",
+    "url": "/simple-calculator/static/js/2.abc42593.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/simple-calculator/static/js/2.abc42593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d3e135115131437979",
+    "url": "/simple-calculator/static/js/main.3d6c9af7.chunk.js"
+  },
+  {
+    "revision": "79f945748914fe2d93da",
+    "url": "/simple-calculator/static/js/runtime-main.dd4b4cde.js"
+  }
+]);
